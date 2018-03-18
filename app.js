0ab9e42c286d5648ce8d65582ee450ca186e3468
RanNiv <35506862+RanNiv@users.noreply.github.com>
2018-03-18 20:55:01 +0200

    Update app.file

--- app.js
+++ app.js
@@ -1,74 +1,56 @@
-var fs = require("fs");  //fs = file-system
-var http = require("http");
+let fs = require("fs");  //fs = file-system
+let http = require("http");
 
 
-var content="<h1>default msg</h1>";
+let content="<h1>default msg</h1>";
 let datePartHmlString="morning";
+let currentDateTime = new Date(); 
 
-
-var currentDateTime = new Date(); 
-var day=currentDateTime.getDate();
-var month=currentDateTime.getMonth();
-var year=currentDateTime.getFullYear();
-
-var CurrentDate=`${year}-${month+1}-${day}`;
-var tomorrowDate=`${year}-${month+1}-${day+1}`;
-
-
-
-let morning= {
-     htmlFile:"morning",
-     startTime: " 06:00:00",
-     endTime:" 12:00:00",
-     
+switch(currentDateTime.getHours()) {
+case 6:
+case 7:
+case 8:
+case 9:
+case 10:
+case 11:
+datePartHmlString="morning";break;
+case 12:
+case 13:
+case 14:
+case 15:
+datePartHmlString="afternoon";break;
+case 16:
+case 17:
+case 18:
+case 19:
+datePartHmlString="evening";break;
+case 20:
+case 21:
+case 22:
+case 23:
+case 0:
+case 1:
+case 2:
+case 3:
+case 4:
+case 5:
+datePartHmlString="night";
 }
 
-let afternoon= {
-    htmlFile: "afternoon",
-    startTime: " 12:00:00",
-    endTime: " 16:00:00"
-}
 
-let evening= {
-    htmlFile: "evening",
-    startTime : " 16:00:00",
-    endTime: " 20:00:00"
-}
-
-let night= {
-    htmlFile: "night",
-    startTime: " 20:00:00",
-    endTime: " 06:00:00"
-}
-
-let DatePartsArray=[];
-DatePartsArray.push(JSON.stringify(morning));
-DatePartsArray.push(JSON.stringify(afternoon));
-DatePartsArray.push(JSON.stringify(evening));
-DatePartsArray.push(JSON.stringify(night));
-
-for (let datepart of DatePartsArray) {
-
-let dateCheckStart=new Date (CurrentDate+JSON.parse(datepart).startTime);
-let dateCheckEnd=JSON.parse(datepart).htmlFile=="night"?new Date (tomorrowDate+JSON.parse(datepart).endTime):new Date (CurrentDate+JSON.parse(datepart).endTime);
-
-if (currentDateTime>=dateCheckStart && currentDateTime<dateCheckEnd ) {   
-datePartHmlString=JSON.parse(datepart).htmlFile+".html";
-
-break;
-}
-}
+datePartHmlString+=".html";
 
 
 fs.readFile("./htmlpages/"+datePartHmlString,"utf-8", function(err, data){
 	content=data;
-})
+});
 
+console.log("listen on http://localhost:3500/");
 
 // Create http server:
 var myServer = http.createServer(function(request, response){
-	
-	console.log("User requested page...");
+    
+    
 	
 	// Send back header:
 	response.writeHead(200, {"Content-Type": "text/html"});
